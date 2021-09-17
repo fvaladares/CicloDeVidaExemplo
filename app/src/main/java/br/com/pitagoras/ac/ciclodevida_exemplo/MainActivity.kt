b6faef24c52b4c7f2f10198b895853dda5beeb3a
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-09-17 20:16:30 -0300

    Implementação dos callbacks

--- app/src/main/java/br/com/pitagoras/ac/ciclodevida_exemplo/MainActivity.kt
+++ app/src/main/java/br/com/pitagoras/ac/ciclodevida_exemplo/MainActivity.kt
@@ -1,11 +1,57 @@
 package br.com.pitagoras.ac.ciclodevida_exemplo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import br.com.pitagoras.ac.ciclodevida_exemplo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
+        Log.d(TAG, "onCrete()")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy()")
+    }
+
+
+    companion object {
+        const val TAG = "[DDM]"
+    }
+
 }
